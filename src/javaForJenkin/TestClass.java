0c32c9977941d143bfc9f3573fa9d1c3d6ca3a33
unknown <khanrobin12@yahoo.com>
2019-05-23 13:31:34 -0400

    test for jenkin

---
+++ src/javaForJenkin/TestClass.java
@@ -0,0 +1,12 @@
+package javaForJenkin;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("i am in love");
+		System.out.println("i like you");
+	}
+
+}
